9dd271f1b1bd7d32cf734c0f506f4f9f9d96439e
luka <lukakakhniashvili0@gmail.com>
2025-11-22 10:55:20 +0400

    Update user.route.js

--- users/user.route.js
+++ users/user.route.js
@@ -2,12 +2,24 @@ const { Router } = require("express");
 const userModel = require("../models/users.model");
 const { upload, deleteFromCloudinary } = require("../config/clodinary.config");
 const postModel = require("../models/post.model");
-const isAuth = require("../middlewares/isauth.middleware"); // << make sure this is here
+const isAuth = require("../middlewares/isauth.middleware"); // make sure auth middleware is here
 
 const userRouter = Router();
 
+// GET current logged-in user
+userRouter.get("/current-user", isAuth, async (req, res) => {
+  try {
+    const user = await userModel.findById(req.userId);
+    if (!user) return res.status(404).json({ error: "User not found" });
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to fetch user" });
+  }
+});
+
 // GET all users
-userRouter.get("/", async (req, res) => {
+userRouter.get("/", isAuth, async (req, res) => {
   const users = await userModel.find().sort({ _id: -1 });
   res.status(200).json(users);
 });
@@ -25,6 +37,9 @@ userRouter.put("/", isAuth, upload.single("avatar"), async (req, res) => {
       user.avatar = req.file.path; // Cloudinary URL
       user.avatarPublicId = req.file.filename; // optional
     }
+
+    // Also allow updating avatar via JSON (from frontend PUT request without file)
+    if (req.body.avatar) user.avatar = req.body.avatar;
 
     await user.save();
     res.json({ message: "Profile updated successfully", user });
@@ -53,6 +68,3 @@ userRouter.delete("/:id", isAuth, async (req, res) => {
 });
 
 module.exports = userRouter;
-
-
-// Express backend route
