7bd92744745ff904b3539e2c66cc7d177e4acb7c
luka <lukakakhniashvili0@gmail.com>
2025-11-28 13:22:09 +0400

    Update user.validation.js

--- validations/user.validation.js
+++ validations/user.validation.js
@@ -1,13 +1,10 @@
-const Joi = require('joi')
+const Joi = require('joi');
 
 const userSchema = Joi.object({
     fullname: Joi.string().min(4).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).max(20).required(),
-    accountType: Joi.string().valid("user", "admin").optional() // allow it
+    accountType: Joi.string().valid("user", "admin").default("user") // optional, defaults to user
 });
 
-
-
-
-module.exports = userSchema
+module.exports = userSchema;
