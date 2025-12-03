7ba35470a29f694519c452bfa6f6e2774cf03aac
bugabugabugabuga <Andria.sulkhanishvili@gmail.com>
2025-12-04 01:35:47 +0400

    delete

--- posts/post.route.js
+++ posts/post.route.js
@@ -49,4 +49,20 @@ postRouter.post("/", isAuth, upload.single("image"), async (req, res) => {
   }
 });
 
+postRouter.delete('/:id', async (req, res) => {
+    const {id} = req.params
+    if(!isValidObjectId(id)){
+        return res.status(400).json({message: "id is invalid"})
+    }
+
+    const post = await postModel.findById(id)
+
+    if(post.author.toString() !== req.userId){
+        return res.status(401).json({message: 'you dont have permition'})
+    }
+
+    await postModel.findByIdAndDelete(id)
+    res.status(200).json({message: "post deleted successfully"})
+})
+
 module.exports = postRouter;
