4a07d2440e6082ee62162169a7076787db86782c
luka <lukakakhniashvili0@gmail.com>
2026-01-14 19:55:33 +0400

    Update order.model.js

--- models/order.model.js
+++ models/order.model.js
@@ -5,10 +5,10 @@ const orderSchema = new mongoose.Schema(
     sessionId: { type: String, required: true },
     status: { type: String, enum: ["PENDING", "SUCCESS", "REJECTED"], default: "PENDING" },
     amount: { type: Number, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // donor
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true }, // donor
     report: { // only report now
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Report",
+      ref: "report",
       required: true,
     },
     paymentIntentId: String,
