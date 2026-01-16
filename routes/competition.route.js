e54b5e109ce0104bb4ec396200daf61ee4bb19e7
bugabugabugabuga <Andria.sulkhanishvili@gmail.com>
2026-01-17 01:28:56 +0400

    Update competition.route.js

--- routes/competition.route.js
+++ routes/competition.route.js
@@ -1,24 +1,24 @@
-// const { Router } = require("express");
-// const isAuth = require("../middlewares/isauth.middleware");
-// const Competition = require("../models/competition.model");
+const { Router } = require("express");
+const isAuth = require("../middlewares/isauth.middleware");
+const Competition = require("../models/competition.model");
 
-// const router = Router();
+const router = Router();
 
-// // JOIN COMPETITION
-// router.post("/join", isAuth, async (req, res) => {
-//   try {
-//     const exists = await Competition.findOne({ user: req.userId });
-//     if (exists) {
-//       return res.json({ message: "Already joined" });
-//     }
+// JOIN COMPETITION
+router.post("/join", isAuth, async (req, res) => {
+  try {
+    const exists = await Competition.findOne({ user: req.userId });
+    if (exists) {
+      return res.json({ message: "Already joined" });
+    }
 
-//     await Competition.create({ user: req.userId });
+    await Competition.create({ user: req.userId });
 
-//     res.json({ message: "Joined competition" });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ message: "Failed to join competition" });
-//   }
-// });
+    res.json({ message: "Joined competition" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Failed to join competition" });
+  }
+});
 
-// module.exports = router;
+module.exports = router;
